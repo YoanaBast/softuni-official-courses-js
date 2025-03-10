1061ee0e2b6210a73cba56271f4688e2911176eb
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-03-10 17:01:21 +0200

    solved greaterNum

---
+++ Basics/Conditional Statements - Lab/greaterNum.js
@@ -0,0 +1,15 @@
+function greaterNum(int, int2) {
+    let greater = 0
+
+    if (int > greater) {
+        greater = int
+    }
+
+    if (int2 > greater) {
+        greater = int2
+    }
+
+    console.log(greater);
+}
+
+greaterNum(3,5)
