9e047d0e39775bc0c34a028be15ad8ee7af20bb3
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-03-14 02:46:56 +0200

    solved gozillaVSKong

---
+++ Basics/Conditional Statements - Ex/gozillaVsKong.js
@@ -0,0 +1,24 @@
+function count(budget, peopleQ, peopleP) {
+    let decorP = 0.10 * budget;
+    let clothes = peopleP * peopleQ;
+
+    if (peopleQ > 150) {
+        clothes -= clothes * 0.10;  //dicount 10%
+    }
+
+    let expenses = decorP + clothes;
+
+    if (expenses < budget) {
+        console.log('Action!');
+        console.log(`Wingard starts filming with ${(budget - expenses).toFixed(2)} leva left.`);
+    } else {
+        console.log("Not enough money!");
+        console.log(`Wingard needs ${(expenses - budget).toFixed(2)} leva more.`)
+    }
+}
+
+count(20000,
+
+120,
+
+55.5)
