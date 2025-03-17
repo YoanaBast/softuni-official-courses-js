bf1f8aa9f9b2eb96594a035537f7c0782674038b
Yoana Bastiyanova <bastianovajoana@gmail.com>
2025-03-17 05:45:06 +0200

    git uwu

---
+++ Basics/Conditional Statements Advanced - Lab/invalidNum.js
@@ -0,0 +1,16 @@
+function solve(num) {
+let isvalid = true;
+
+if (num === 0 || num >= 100 && num <= 200) {
+    isvalid;
+} else {
+    isvalid =  false;
+}
+
+if (isvalid === false) {
+    console.log('invalid');
+}
+
+}
+
+solve(75)
